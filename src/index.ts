8be13c88523d85ff1a60c1c517a664aaddfe03d0
MrCsabaToth <csaba.toth.us@gmail.com>
2024-08-03 22:14:47 -0700

    Adding token check and also verify the query is there

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,18 @@ import * as ff from '@google-cloud/functions-framework';
 import main from "./main"
 
 ff.http('DuckDuckGoFunction', (req: ff.Request, res: ff.Response) => {
-  main(req, res)
+  const url = new URL(req.url)
+  const params = new URLSearchParams(url.search)
+  const token = params.get('token')
+  const accesstToken = process.env.ACCESS_TOKEN
+  if (token !== undefined && token && accesstToken !== undefined && token === accesstToken) {
+    res.status(401)
+  } else {
+    const query = params.get('q') || ''
+    if (query.trim()) {
+      main(req, res)
+    } else {
+      res.status(400)
+    }
+  }
 })
